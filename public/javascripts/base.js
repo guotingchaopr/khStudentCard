ab6fc50f40493eabddb5c4adfd122e6c914cfb62
guotingchaopr <guotingchaopr@gmail.com>
2015-04-26 16:19:33 +0800

    add video stream

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -1,4 +1,25 @@
 $(function(){
 	$(".ui.radio.checkbox").checkbox();
 	$('.selection.dropdown').dropdown();
+	var videoObj = {video:true},
+		current_stream ;
+	$("#photo").click(function(){
+		$("#phto_Modal").modal({
+		    closable  : false,
+		    onDeny    : function(){
+		      return false;
+		    },
+		    onApprove : function() {
+		      current_stream.stop();
+		    }
+	  	}).modal("show");
+			navigator.webkitGetUserMedia({video:true},function (stream) {
+			 	$("#mediaStream")[0].src = URL.createObjectURL(stream);
+			 	current_stream=stream;
+			},
+			function (err) {
+				console.log(err);
+			}
+		);
+	});
 })
